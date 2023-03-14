47e8067d3cf362eac84de3b6cf3845ba9cd95178
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-03-14 23:08:26 +0530

    Create shorthand_ifelse.py

---
+++ shorthand_ifelse.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+if a>b:
+    print("a is greater than b ")
+else:
+    print("b is greater than a")
+
+#shorthand if else
+c = int(input())
+d = int(input())
+print("cis greater than d") if c>d else print("d is greater than c")
