3b97ee4a2fd80706de696d25561c22b9d5070555
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-03-15 23:44:45 +0530

    Create File IO Basics.py

---
+++ File IO Basics.py
@@ -0,0 +1,11 @@
+#File IO basics
+#non volatile memory is stored in files
+"""
+r - helps to read from a a file
+w - helps to write to a file
+x - helps to create a new file in case its not present; else fails
+a - add more content to a file(append to the end)
+t - text mode (file would store text files or content
+b - binary mode
++ - read and write happens (for the purpose of update)
+"""
