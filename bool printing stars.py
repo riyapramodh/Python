4452b5b7efb7aeec01e059307925971f3989f9ea
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-03-16 23:27:24 +0530

    Create bool printing stars.py

---
+++ bool printing stars.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number:"))
+b = bool(input("Enter true or False:"))
+if b == True:
+    print("*")
+    print("**")
+    print("***")
+    print("****")
+    print("*****")
+else:
+    print("*****")
+    print("****")
+    print("***")
+    print("**")
+    print("*")
