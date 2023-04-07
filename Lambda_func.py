d89c4c77938d317c17b42cde65874abfb9cb25d7
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-04-07 11:03:30 +0530

    Update Lambda_func.py

--- Lambda_func.py
+++ Lambda_func.py
@@ -1,4 +1,5 @@
 #lambda or anonymous functions
+#EXAMPLE 1
 #normal function
 def add(x,y):
     return x+y
@@ -7,3 +8,17 @@ print(add(10,5))
 #lambda function
 sub = lambda x,y :x-y
 print(sub(10,5))
+
+#EXAMPLE 2
+#normal way:
+def func(a):
+    return a[1]
+
+a = [[1,14], [20,3], [2,44]]
+a.sort(key = func )
+print(a)
+
+#lambda function way
+a = [[1,14], [20,3], [2,44],[2,2]]
+a.sort(key = lambda a:a[1])
+print(a)
